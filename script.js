c6bf545d00889f9be50db32ed8cece6d81d6c5cc
imasha100 <121811208+imasha100@users.noreply.github.com>
2024-03-17 20:35:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed= new Typed(".text", {
+    strings:["BSC(hons)Software Engineering Undergraduate" , "BSC(hons)Software Engineering Undergraduate" , "BSC(hons)Software Engineering Undergraduate"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+
+});
